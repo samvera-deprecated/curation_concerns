a88eebabf112d5226f3f4510cd58fcb4db976ce2
Kevin Reiss <kevin.reiss@gmail.com>
2015-05-14 14:20:56 -0400

    remove reference to permissions load in curatable

--- worthwhile-models/app/models/concerns/curation_concern/curatable.rb
+++ worthwhile-models/app/models/concerns/curation_concern/curatable.rb
@@ -5,8 +5,9 @@ module CurationConcern::Curatable
   included do
     include ::CurationConcern::HumanReadableType
     include CurationConcerns::Noid
-    include CurationConcerns::Permissions::Readable
-    include CurationConcerns::Permissions::Writable
+    # leaving these in causes generic_work_specs to fail
+    # include CurationConcerns::Permissions::Readable
+    # include CurationConcerns::Permissions::Writable
     include Sufia::ModelMethods
     include Hydra::Collections::Collectible
     include Solrizer::Common
