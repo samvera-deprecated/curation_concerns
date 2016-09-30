7268e27ff196636096ef5cab08c0ae64c9c27717
Joe Atzberger <ohiocore@gmail.com>
2016-09-29 18:56:09 -0700

    Slim down CollectionSearchBuilder
    Updates made possible by `filter_by_type` improvements.
    Initializer takes array of permissions now, and we'll actually check the
    parameter for once.

--- app/search_builders/curation_concerns/collection_search_builder.rb
+++ app/search_builders/curation_concerns/collection_search_builder.rb
@@ -8,15 +8,19 @@ module CurationConcerns
     #   From a controller, scope would be `self`.  This argument passed to ::SearchBuilder.new()
     # @param [Symbol] permissions defining breadth of search, e.g. :edit, :read
     # @note permissions will otherwise be defaulted by inherited #discovery_permissions
+    # @see @discovery_permissions
+    # @see @discovery_permissions
     def initialize(scope, access = nil)
       @rows = 100
       @discovery_permissions ||= access_levels[access] if access
       super(scope)
     end
 
-    # @return [String] class URI of the model, as indexed in Solr has_model_ssim field
-    def model_class_uri
-      ::Collection.to_class_uri
+    # @!group Overrides
+    #
+    # @return [Class] ActiveFedora model(s) indexed in Solr has_model_ssim field
+    def models
+      [::Collection]
     end
 
     # @return [String] Solr field name indicating default sort order
@@ -24,25 +28,14 @@ module CurationConcerns
       'title_si'
     end
 
-    # @return [Hash{Symbol => Array[Symbol]}] bottom-up map of "what you need" to "what qualifies"
-    # @note i.e., requiring :read access is satisfied by either :read or :edit access
-    def access_levels
-      { read: [:read, :edit], edit: [:edit] }
-    end
+    # @!endgroup
 
-    # @!group Overrides
-    #
-    # This overrides the filter_models in FilterByType
-    def filter_models(solr_parameters)
-      solr_parameters[:fq] ||= []
-      solr_parameters[:fq] << ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: model_class_uri)
-    end
+    private
 
-    # Sort results by title if no query was supplied.
-    # This overrides the default 'relevance' sort.
-    def add_sorting_to_solr(solr_parameters)
-      return if solr_parameters[:q]
-      solr_parameters[:sort] ||= "#{sort_field} asc"
-    end
+      # @return [Hash{Symbol => Array[Symbol]}] bottom-up map of "what you need" to "what qualifies"
+      # @note i.e., requiring :read access is satisfied by either :read or :edit access
+      def access_levels
+        { read: [:read, :edit], edit: [:edit] }
+      end
   end
 end
