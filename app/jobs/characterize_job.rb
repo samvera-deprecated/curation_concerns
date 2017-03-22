d8487a06b5f1a26743dd15f3fafd4072c0149277
Alex Dunn <dunn.alex@gmail.com>
2017-03-22 11:05:07 -0700

    jobs: lock FileSets while attaching Files
    Currently
    ```
    actor = CurationConcerns::Actors::FileSetActor.new(file_set, user)
    actor.create_content('file1')
    actor.create_content('file2', 'restored')
    ```
    
    Can fail due to background jobs trying to create and/or modify the FileSet at the same time.

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -1,4 +1,6 @@
 class CharacterizeJob < ActiveJob::Base
+  include CurationConcerns::Lockable
+
   queue_as CurationConcerns.config.ingest_queue_name
 
   # @param [FileSet] file_set
@@ -7,11 +9,16 @@ class CharacterizeJob < ActiveJob::Base
   def perform(file_set, file_id, filepath = nil)
     filename = CurationConcerns::WorkingDirectory.find_or_retrieve(file_id, file_set.id, filepath)
     raise LoadError, "#{file_set.class.characterization_proxy} was not found" unless file_set.characterization_proxy?
-    Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filename)
-    Rails.logger.debug "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
-    file_set.characterization_proxy.save!
-    file_set.update_index
-    file_set.parent.in_collections.each(&:update_index) if file_set.parent
+
+    # Prevent other jobs from trying to modify the FileSet at the same time
+    acquire_lock_for(file_set.id) do
+      Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filename)
+      Rails.logger.debug "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
+      file_set.characterization_proxy.save!
+      file_set.update_index
+      file_set.parent.in_collections.each(&:update_index) if file_set.parent
+    end
+
     CreateDerivativesJob.perform_later(file_set, file_id, filename)
   end
 end
